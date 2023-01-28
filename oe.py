b5237b950b5b80b55e89f2da2c577f347dbe9766
Paul <fenixs.paul@gmail.com>
2023-01-28 20:56:28 +0200

    add file

--- oe.py
+++ oe.py
@@ -4,8 +4,8 @@ import datetime
 def main():
     t = datetime.datetime.now().strftime('%d_%m_%Y_%H_%M')
     r = requests.get('https://energypay.oe.if.ua:8889/GAVTurnOff/'
-                     'CityFilterPReport?cityList=11726&remList=nad_p&dBegin=&dEnd=&allRem=false')
-    with open(f'file/{t}.html', 'wb') as f:
+                     'CityFilterPReport?cityList=11726&remList=nad_p&dBegin=&dEnd=&allRem=false', verify=False)
+    with open(f'files/{t}.html', 'wb') as f:
         f.write(r.content)
 
 if __name__ == "__main__":
